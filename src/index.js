3f9248f17536f7256763d8ac584acdab258c1931
+ <git-commit-mcreasmith23@gmail.com>
2021-08-17 16:24:31 -0500

    src: pulling api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      'items': []
+    }
+  }
+
+  componentDidMount() {
+    this.getItems();
+  }
+  
+  getItems() {
+    fetch('https://randomuser.me/api/')
+    .then(results => results.json())
+    .then(results => this.setState({'items': results}));
+  }
+
+  render() {
+    return (
+      <h1> Test</h1>
+    );
+  }
+}
+
+ReactDom.render(
+  <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
