d9be26b67ba4bfef19172542f652e2270982ba27
+ <git-commit-mcreasmith23@gmail.com>
2021-08-23 20:46:18 -0500

    components: claw added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Header.css'
 import lobster from './images/lobster.png'
-
+import claw from './images/claw.png'
 const pic = lobster
+const pic2 = claw
 
 const Header = () => {
   return (
     <header>
       <h1> <img src= {pic} alt="" className="Lob"/> </h1>
       <h4> LOBSTER MARKETING DEV TEST </h4>
+      <h2> <img src= {pic2} alt="" className="Claw"/> </h2>
       <div className="body-content"> 
         <h2> I'd &hearts; to work here! </h2>
           <p> I believe Lobster Marketing would be an amazing opportunity for to work out. I know I would fit in with the team culture. I am hard-working and ready to start my future with your company. Give me the chance to be a part of your team!  </p>
