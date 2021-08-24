3297d92c1e16d21a5e282c1e524523cbe1ba5cd2
+ <git-commit-mcreasmith23@gmail.com>
2021-08-23 20:41:13 -0500

    components/Header: final touches

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,14 @@ const pic = lobster
 
 const Header = () => {
   return (
-      <header>
-          <h1> <img src= {pic} alt="" className="Lob"/> </h1>
-          <h4> LOBSTER MARKETING DEV TEST </h4>
-      </header>
+    <header>
+      <h1> <img src= {pic} alt="" className="Lob"/> </h1>
+      <h4> LOBSTER MARKETING DEV TEST </h4>
+      <div className="body-content"> 
+        <h2> I'd &hearts; to work here! </h2>
+          <p> I believe Lobster Marketing would be an amazing opportunity for to work out. I know I would fit in with the team culture. I am hard-working and ready to start my future with your company. Give me the chance to be a part of your team!  </p>
+      </div>
+    </header>
   )
 }
 
